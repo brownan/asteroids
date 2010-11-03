5489b4f9a0def813ba0ae8f14a73b983b069580d
Andrew Brown <brownan@gmail.com>
2010-11-03 10:25:30 -0400

    added material support from obj files

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from OpenGL.GL import *
 
 import random
 import numpy
+from collections import defaultdict
 
 class Model(object):
     """A model that can be drawn on the screen"""
@@ -10,38 +11,73 @@ class Model(object):
         functions, and glEnd"""
         raise NotImplementedError()
 
+class _Material(object):
+    """Defines a particular material"""
+    def __init__(self, ambient, diffuse, specular=(0,0,0,0), emission=(0,0,0,0)):
+        assert len(ambient)==4
+        assert len(diffuse)==4
+        assert len(specular)==4
+        assert len(emission)==4
+        self.ambient = ambient
+        self.diffuse = diffuse
+        self.specular = specular
+        self.emission = emission
+
+    def activate(self):
+        glMaterialfv(GL_FRONT, GL_AMBIENT, self.ambient)
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, self.diffuse)
+        glMaterialfv(GL_FRONT, GL_SPECULAR, self.specular)
+        glMaterialfv(GL_FRONT, GL_EMISSION, self.emission)
+
 class ObjModel(Model):
     """A model loaded from an obj file"""
     def __init__(self, fileobj):
         if isinstance(fileobj, (str, unicode)):
             fileobj = open(fileobj, 'r')
 
+        # Internal list of ordered points referenced by the face definitions.
+        # These arrays start at 1.
         self.vertices = [None]
         self.normals = [None]
 
-        # list of polygons
-        # each item is a list of (vertex, normal), where normal and vertex are
-        # indicies into the normal and vertices array
-        self.triangles = []
-        self.quads = []
-        self.polys = []
+        # The material map. Maps material names to _Material objects
+        self.mats = {}
+
+        # Stores all the ploygons for this model
+        # Keys are material names
+        # Each value is a polygon list
+        # Each polygon list item is a list of (vertex, normal), where normal
+        # and vertex are numpy vectors. Together, the list of points define a
+        # single polygon
+        self.polys = defaultdict(list)
+        currentmat = None
 
         for line in fileobj:
             if not line.strip():
                 continue
+            lineparts = line.strip().split()
 
-            if line[0] == "v":
+            if lineparts[0] == "v":
                 # Define a vertex
                 _, p1, p2, p3 = line.split()
                 self.vertices.append(numpy.array((float(p1), float(p2), float(p3))))
 
-            if line[0:2] == "vn":
+            elif lineparts[0] == "vn":
                 _, p1, p2, p3 = line.split()
                 self.normals.append(numpy.array((float(p1), float(p2), float(p3))))
 
-            if line[0] == "f":
+            elif lineparts[0] == "mtllib":
+                for filename in lineparts[1:]:
+                    self._parsemat(filename)
+
+            elif lineparts[0] == "usemtl":
+                currentmat = self.mats[line.split()[1]]
+
+            elif lineparts[0] == "f":
                 # Define a single polygon
                 face_components = line.split(None, 1)[1].split()
+                # Go over each point in this polygon and gather them in this
+                # list
                 points = []
                 for component in face_components:
                     # one vertex component of this polygon
@@ -50,53 +86,60 @@ class ObjModel(Model):
                     normalv = self.normals[int(normal)]
                     points.append((vertex, normalv))
 
-                if len(points) == 3:
-                    self.triangles.append(points)
-                elif len(points) == 4:
-                    self.quads.append(points)
-                else:
-                    self.polys.append(points)
+                # Now that all points have been parsed and mapped for this
+                # polygon, put it in the polygon list for the current material
+                self.polys[currentmat].append(points)
+
+    def _parsemat(self, matfilename):
+        f = open(matfilename, 'r')
+        try:
+
+            matname = None
+            mat = None
+            for line in f:
+                lineparts = line.strip().split()
+                if not lineparts:
+                    continue
+
+                if lineparts[0] == "newmtl":
+                    # Save existing mat if one was defined already
+                    if matname:
+                        print "saved mat", matname
+                        self.mats[matname] = _Material(*mat)
+
+                    # Start a new material
+                    matname = line.split()[1]
+                    # Stores four quadruplets: ambient, diffuse, specular, emission
+                    mat = [(0,0,0,0)] * 4
+
+                elif lineparts[0] in ("Ka", "Kd", "Ks", "Ke"):
+                    m = {'a':0, 'd':1, 's': 2, 'e': 3}
+                    color = [float(x) for x in line.split()[1:]]
+                    if len(color) == 1:
+                        color *= 3
+                        color.append(0.0)
+                    elif len(color) == 3:
+                        color.append(0.0)
+
+                    mat[m[line[1]]] = color
+            # Save the final one
+            if matname:
+                print "saved mat", matname
+                self.mats[matname] = _Material(*mat)
+        finally:
+            f.close()
 
     def draw(self):
-        # Draw triangles
-        if self.triangles:
-            glBegin(GL_TRIANGLES)
-            for tri in self.triangles:
-                (p1,n1),(p2,n2),(p3,n3) = tri
-                glNormal3dv(n1)
-                glVertex3dv(p1)
-                glNormal3dv(n2)
-                glVertex3dv(p2)
-                glNormal3dv(n3)
-                glVertex3dv(p3)
-            glEnd()
-
-        # Draw quadralaterals
-        if self.quads:
-            glBegin(GL_QUADS)
-            for quad in self.quads:
-                (p1,n1),(p2,n2),(p3,n3),(p4,n4) = quad
-                #print "->%s\n  %s\n  %s\n  %s" % (n1,n2,n3,n4)
-                glNormal3dv(n1)
-                glVertex3dv(p1)
-
-                glNormal3dv(n2)
-                glVertex3dv(p2)
-
-                glNormal3dv(n3)
-                glVertex3dv(p3)
-
-                glNormal3dv(n4)
-                glVertex3dv(p4)
-            glEnd()
-
         # Draw polygons
-        for p in self.polys:
-            glBegin(GL_POLYGON)
-            for pt, n in p:
-                glNormal3dv(n)
-                glVertex3dv(pt)
-            glEnd()
+        for texture, polygons in self.polys.iteritems():
+            if texture:
+                texture.activate()
+            for p in polygons:
+                glBegin(GL_POLYGON)
+                for pt, n in p:
+                    glNormal3dv(n)
+                    glVertex3dv(pt)
+                glEnd()
 
 
 class AsteroidModel(ObjModel):
@@ -108,70 +151,3 @@ class AsteroidModel(ObjModel):
         for vertex in self.vertices[1:]:
             vertex *= random.uniform(0.7,1.3)
 
-class MichaelAsteroidModel(Model):
-    asteroid_verticies = numpy.array([
-            [0.0, 0.0, 1.0], # Top
-
-            # Upper Ring
-            [0.0, 1.0, 0.33], # 1
-            [-0.66, 0.66, 0.33],
-            [-1.0, 0.0, 0.33],
-            [-0.66, -0.66, 0.33],
-            [0.0, -1.0, 0.33],
-            [0.66, -0.66, 0.33],
-            [1.0, 0.0, 0.33],
-            [0.66, 0.66, 0.33], # 8
-
-            [0.0, 0.0, -1.0], # Bottom
-
-            # Lower Ring
-            [0.0, 1.0, -0.33], # 10
-            [-0.66, 0.66, -0.33],
-            [-1.0, 0.0, -0.33],
-            [-0.66, -0.66, -0.33],
-            [0.0, -1.0, -0.33],
-            [0.66, -0.66, -0.33],
-            [1.0, 0.0, -0.33],
-            [0.66, 0.66, -0.33]
-        ])
-    def __init__(self):
-        raise NotImplementedError()
-        max_variance = 0.3
-        min_variance = 0.2
-        variance = random.uniform(min_variance, max_variance)
-        
-        self.vertices = self.asteroid_verticies.copy()
-        for (x,y), value in numpy.ndenumerate(self.vertices):
-            self.vertices[x,y] += random.uniform(-variance, variance)
-
-    def _v(self, n):
-        glNormal3dv(self.vertices[n])
-        glVertex3d(-self.vertices[n,0], -self.vertices[n,1], -self.vertices[n,2])
-
-    def draw(self):
-        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, [0.2, 0.2, 0.2])
-
-        v = self._v
-
-        # top fan
-        glBegin(GL_TRIANGLE_FAN)
-        for i in xrange(9):
-            v(i)
-        v(1)
-        glEnd()
-
-        glBegin(GL_TRIANGLES)
-        for i in xrange(1,8):
-            v(i); v(i+9); v(i+1)
-            v(i+1); v(i+9); v(i+10)
-        v(8); v(17); v(1)
-        v(1); v(17); v(10)
-        v(1); v(10); v(10);
-        v(10); v(10); v(11);
-        glEnd()
-
-        glBegin(GL_TRIANGLE_FAN)
-        for i in xrange(9, 18):
-            v(i)
-        v(10)
-        glEnd()
